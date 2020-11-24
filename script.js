90ff663ee962827f92e01892a204003026367a35
elsasan01 <esan5574@uni.sydney.edu.au>
2020-11-24 05:49:15 +0000

    Added a scroll back up button

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+// hamburger menu
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+// Scroll back up button
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// Projects vertical tabs
 function openProject(evt, projectName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -18,5 +34,7 @@ function openProject(evt, projectName) {
   document.getElementById(projectName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 document.getElementById("defaultOpen").click();
+
+
+
